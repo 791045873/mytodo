6cdb6d03639f982b9f5d9fb2aa0ab2fcb4a87e4b
wangkaiyuan <791045873@qq.com>
2017-08-18 23:58:16 +0800

    app

--- src/container/App.js
+++ src/container/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import input from '../container/input';
-import show from '../container/show';
+import Input from '../container/inputContainer';
+import ShowList from '../container/showListContainer';
+import ShowControl from '../container/showControlContainer'
 
 class App extends Component {
   render() {
     return (
       <div>
-          <input />
-          <show />
+          <Input />
+          <ShowList />
+          <ShowControl/>
       </div>
     );
   }
